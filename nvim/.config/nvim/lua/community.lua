1b88436f824982a597caa689523f492e67f31299
Anurag <86455065+theAnuragMishra@users.noreply.github.com>
2025-02-28 01:10:53 +0530

    add zig astrocommunity pack

--- nvim/.config/nvim/lua/community.lua
+++ nvim/.config/nvim/lua/community.lua
@@ -23,5 +23,7 @@ return {
   { import = "astrocommunity.pack.java" },
   { import = "astrocommunity.pack.go" },
   { import = "astrocommunity.pack.astro" },
+  { import = "astrocommunity.pack.zig" },
+  { import = "astrocommunity.markdown-and-latex.glow-nvim" },
   -- import/override with your plugins folder
 }
